e90e2bd6a444ed762e0cfead02d5664f774f0e3b
Dmitriy Karzakov <dk@didlogic.com>
2025-01-26 18:11:06 +0300

    added ShortUrl client component

---
+++ src/views/home/ShortUrl/index.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form"
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+
+import { urlRegex } from "@/lib/helper"
+
+import type { FC } from "react"
+
+const FormSchema = z.object({
+  link: z
+    .string()
+    .nonempty({
+      message: "Введите ссылку",
+    })
+    .regex(urlRegex, {
+      message: "Введите корректную ссылку",
+    }),
+})
+
+export const ShortUrl: FC = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      link: "",
+    },
+  })
+
+  const onSubmit = (data: z.infer<typeof FormSchema>) => {
+    console.log(data)
+  }
+
+  return (
+    <div className="container lg:flex lg:items-center lg:mt-[-64px] lg:h-[100vh] md:max-w-2xl w-full pt-20 lg:pt-0">
+      <Form {...form}>
+        <form
+          className="flex w-full sm:items-center gap-x-2 gap-y-8 flex-col sm:flex-row"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="link"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    placeholder="Вставьте ссылку для сокращения"
+                    className="bg-gray-100 h-[56px]"
+                    autoFocus
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage className="absolute" />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="h-[56px]">
+            Сократить
+          </Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
---
+++ src/widgets/RootHeader/index.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+import { IconLink } from "@tabler/icons-react"
+
+import { HOMEPAGE_ROUTE } from "@/routes"
+
+import type { FC } from "react"
+
+export const RootHeader: FC = () => {
+  return (
+    <div className="container">
+      <div className="flex h-12 md:h-16 items-center justify-between border-b-slate-300 border-b">
+        <div className="flex items-center gap-2 cursor-pointer text-gray-800">
+          <IconLink size={24} />
+          <Link href={HOMEPAGE_ROUTE} className="font-bold">
+            LSHORT
+          </Link>
+        </div>
+
+        <div className="hidden sm:block">Бесплатный сокращатель ссылок</div>
+      </div>
+    </div>
+  )
+}
