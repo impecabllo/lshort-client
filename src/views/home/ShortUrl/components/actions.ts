eaeb716979b8caecc17dad8828e678d1dc1cf261
Dmitriy Karzakov <impecabllo@gmail.com>
2025-02-04 18:15:57 +0300

    added credentials

--- src/views/home/ShortUrl/components/actions.ts
+++ src/views/home/ShortUrl/components/actions.ts
@@ -18,6 +18,7 @@ export async function createShortUrl(formData: CreateShortUrlData) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(payload),
+    credentials: "include",
   })
 
   return response.json()
